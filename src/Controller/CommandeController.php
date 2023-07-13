163df4301db2e266004eb99a8b6f9d942cd5564c
Romeo Djoman <cra.djoman@gmail.com>
2023-07-13 16:51:01 +0200

    version 1.5

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commande;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommandeController extends AbstractController
 {
@@ -16,7 +18,7 @@ class CommandeController extends AbstractController
         }
 
         $commande = new Commande(); 
-        
+
 
 
 
@@ -24,7 +26,5 @@ class CommandeController extends AbstractController
             'controller_name' => 'CommandeController',
         ]);
     }
-
-    $
     
 }
